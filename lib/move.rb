8d124dcb12b2264ad3614d964e4f832fea3f743e
Chris Baum <christopher.baum@valpo.edu>
2020-12-28 15:20:28 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,6 @@ def input_to_index(user_input)
   converted_input = user_input.to_i - 1 #converts to integer and subtracts 1
 end
 
-def move(board, index, "X")
+def move(board, index, value)
   board[index] = value
 end
